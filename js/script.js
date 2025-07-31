93e97faf7e779f8828bae5f45100a9a9363323ee
Kavkazec <oganesmkrtchyan2004@gmail.com>
2025-07-31 16:02:04 +0300

    add popups add hide unuse blocks

--- js/script.js
+++ js/script.js
@@ -70,3 +70,72 @@ $(document).on('click', '.reviews__item-video .play-button', function(e) {
     // Скрываем кнопку play (если нужно)
     $(this).hide(); 
 });
+
+
+
+//========================== Функционал модальных окон ====================
+
+function openPopup(popup) {
+
+  $('.popup').fadeOut();
+  $('.overlay').fadeIn();
+  $('html').css('overflow', 'hidden');
+  popup.fadeIn();
+}
+
+function closePopup(closeBtn) {
+  $('.overlay').fadeOut();
+  closeBtn.closest('.popup').fadeOut();
+  $('html').css('overflow-y', 'auto');
+}
+
+$(document).ready(function() {
+  $(document).mouseup(function(e) {
+    var container = $('.popup:visible'); // Проверяем только видимые попапы
+    var popupOpenButtons = $('[data-popup]'); // Все кнопки открытия попапов
+    
+    // Если попап видим И клик был не по нему И не по кнопке открытия
+    if (container.length && 
+        !container.is(e.target) && 
+        container.has(e.target).length === 0 &&
+        !popupOpenButtons.is(e.target) && 
+        popupOpenButtons.has(e.target).length === 0) {
+      
+      container.fadeOut();
+      $('.overlay').fadeOut();
+      $('html').css('overflow-y', 'auto');
+    }
+  });
+
+
+  $(document).on('keydown', function(e) {
+    if (e.key === 'Escape' || e.keyCode === 27) {
+      $('.popup').fadeOut();
+      $('.overlay').fadeOut();
+      $('html').css('overflow-y', 'auto');
+    }
+  });
+});
+
+
+$(document).on('click', '.popup-close', function(e) {
+  closePopup($(this));
+});
+
+$(document).on('click', '.popup-get-offer-btn', function(e) {
+  openPopup($('.popup-get-offer'));
+});
+
+$(document).on('click', '.popup-calculator-btn', function(e) {
+  openPopup($('.popup-calculator'));
+});
+
+$(document).on('click', '.popup-cases-btn', function(e) {
+  openPopup($('.popup-cases'));
+});
+
+$(document).on('click', '.popup-lead-magnet-btn', function(e) {
+  openPopup($('.popup-lead-magnet'));
+});
+
+
